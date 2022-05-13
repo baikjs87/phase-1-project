e73e0451f13b6c6aaaf204fbc3dc762075284e23
Jason Baik <baikjs87@gmail.com>
2022-05-13 02:16:57 -0400

    still WIP

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener("submit", searchHero);
 });
 
-function callHeroes() {
-    fetch(
-        "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=9f97a17b4e1fd4bf54be654cb96c7dc6&hash=e207a58c1870cacd8130bd66715662a0&limit=100&offset=100"
-    )
-        .then((result) => result.json())
-        .then((data) => console.log(data.data.results));
-}
+// function callHeroes() {
+//     fetch(
+//         "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=9f97a17b4e1fd4bf54be654cb96c7dc6&hash=e207a58c1870cacd8130bd66715662a0&limit=100&offset=100"
+//     )
+//         .then((result) => result.json())
+//         .then((data) => console.log(data.data.results));
+// }
 
 fetch(
     "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=9f97a17b4e1fd4bf54be654cb96c7dc6&hash=e207a58c1870cacd8130bd66715662a0&limit=100&offset=100"
@@ -36,30 +36,70 @@ function searchHero(e) {
                 if (hero.name.toLowerCase() === searchText) {
                     console.log(hero);
                     showResult(hero);
+                } else {
+                    const errorResult =
+                        document.querySelector(".search-result");
+                    const errorMsg = document.createElement("h1");
+                    errorMsg.className = "error-msg";
+                    errorMsg.textContent = "No Hero Found :(";
+                    console.log(errorMsg);
+                    errorResult.appendChild(errorMsg);
                 }
-                // else {
-                //     const errorResult =
-                //         document.querySelector(".search-result");
-                //     const errorMsg = document.createElement("h1");
-                //     errorMsg.className = "error-msg";
-                //     errorMsg.textContent = "No Hero Found :(";
-                //     console.log(errorMsg);
-                //     errorResult.appendChild(errorMsg);
-                // }
             }
+            // for (const hero of data.data.results) {
+            //     const resultName = hero.name.toLowerCase();
+            //     const obj = { name: resultName };
+            //     const hasVal = Object.values(obj).includes(`${searchText}`);
+            // }
+            // if (hasVal === true) {
+            //     showResult(hero);
+            // } else {
+            //     const errorResult = document.querySelector(".search-result");
+            //     const errorMsg = document.createElement("h1");
+            //     errorMsg.className = "error-msg";
+            //     errorMsg.textContent = "Hero Not Found :(";
+            //     console.log(errorMsg);
+            //     errorResult.appendChild(errorMsg);
+            // }
+
+            // }
+            // for (const hero of data.data.results) {
+            //     if (hero.name.toLowerCase() === searchText) {
+            //         asdf = true;
+            //     }
+            // }
+            // if (asdf === true) {
+            //     console.log(hero);
+            //     showResult(hero);
+            // } else if (asdf === false) {
+            //     const errorResult = document.querySelector(".search-result");
+            //     const errorMsg = document.createElement("h1");
+            //     errorMsg.className = "error-msg";
+            //     errorMsg.textContent = "No Hero Found :(";
+            //     // const msgCount = document.get
+            //     // if(errorMsg < 2)
+            //     console.log(errorMsg);
+            //     errorResult.appendChild(errorMsg);
+            // }
         });
 
     function showResult(data) {
         const searchResult = document.querySelector(".search-result");
 
-        //------ Create search result card
+        //------ Create search result area
         const resultBox = document.createElement("div");
         resultBox.className = "result-box";
 
+        //------ Create details container
+        const detailContainer = document.createElement("div");
+        detailContainer.className = "detail-container";
+
+        //------ Create result hero name
         const resultName = document.createElement("h2");
         resultName.className = "result-hero-name";
         resultName.textContent = `${data.name}`;
 
+        //------ Create result hero description
         const resultDetails = document.createElement("div");
         resultDetails.className = "result-description";
         const heroDesc = document.createElement("p");
@@ -67,6 +107,29 @@ function searchHero(e) {
         resultDetails.className = "hero-description";
         const description = document.createTextNode(`${data.description}`);
 
+        //------ Create comic book list
+        const comicBookBox = document.createElement("div");
+        comicBookBox.className = "comic-book-container";
+        const bookList = document.createElement("ul");
+        bookList.className = "comic-book-list";
+        const bookListTitle = document.createElement("h2");
+        bookListTitle.className = "book-list-title";
+        bookListTitle.textContent = `${data.name} has been featured in these comic books:`;
+        const comicBooks = document.createElement("li");
+        for (const books of data.comics.items) {
+            comicBooks.textContent = `${books.name}`;
+            bookList.appendChild(comicBooks);
+        }
+        comicBookBox.appendChild(bookListTitle);
+        comicBookBox.appendChild(bookList);
+
+        //------ Create result hero link
+        const link = document.createElement("a");
+        link.className = "result-link";
+        link.setAttribute("href", `${data.urls[1].url}`);
+        link.textContent = `More information on ${data.name}!`;
+
+        //------ Create result close button
         const closeBox = document.createElement("button");
         closeBox.className = "close-btn";
         closeBox.textContent = "x";
@@ -82,6 +145,7 @@ function searchHero(e) {
             searchImg.className = "search-image";
         }
 
+        //------ Close search result
         closeBox.addEventListener("click", () => {
             const removeResult = document.querySelector(".result-box");
             removeResult.remove();
@@ -89,10 +153,16 @@ function searchHero(e) {
 
         heroDesc.appendChild(description);
         resultDetails.appendChild(heroDesc);
+
+        detailContainer.appendChild(resultName);
+        detailContainer.appendChild(resultDetails);
+        detailContainer.appendChild(closeBox);
+        detailContainer.appendChild(comicBookBox);
+        detailContainer.appendChild(link);
+
         resultBox.appendChild(searchImg);
-        resultBox.appendChild(resultName);
-        resultBox.appendChild(resultDetails);
-        resultBox.appendChild(closeBox);
+        resultBox.appendChild(detailContainer);
+
         searchResult.appendChild(resultBox);
 
         const form = document.querySelector("#search-form");
@@ -106,7 +176,7 @@ function displayHeroes(hero) {
     const heroList = document.querySelector("#hero-collection");
     const heroCard = document.createElement("div");
     heroCard.className = "card";
-    // heroCard.setAttribute("href", `${hero.urls[0].url}`);
+    // heroCard.addEventListener('click', showResult)
 
     const heroImg = document.createElement("img");
     heroImg.setAttribute(
@@ -122,9 +192,6 @@ function displayHeroes(hero) {
     heroTitle.appendChild(heroName);
     heroCard.appendChild(heroImg);
     heroCard.appendChild(heroTitle);
-    // heroDesc.appendChild(description);
-    // heroDetails.appendChild(heroDesc);
-    // heroCard.appendChild(heroDetails);
 
     heroList.appendChild(heroCard);
 }
